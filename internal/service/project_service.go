ee0db7c3969f73e6f75f7c944ee001bcb726e847
Raj Harsh <80113516+RajHarsh01@users.noreply.github.com>
2023-12-18 14:25:28 +0530

    Update project_service.go

--- internal/service/project_service.go
+++ internal/service/project_service.go
@@ -15,18 +15,22 @@ func NewStudentService(repo *repository.StudentRepository) *StudentService {
 	return &StudentService{Repo: repo}
 }
 
+//Read student base
 func (s *StudentService) GetStudents(ctx *gofr.Context) ([]models.Student, error) {
 	return s.Repo.GetStudents(ctx)
 }
 
+//Create student base
 func (s *StudentService) AddStudent(ctx *gofr.Context, student models.Student) error {
 	return s.Repo.AddStudent(ctx, student)
 }
 
+//Update student base
 func (s *StudentService) UpdateStudent(ctx *gofr.Context, student models.Student) error {
 	return s.Repo.UpdateStudent(ctx, student)
 }
 
+//Delete student base
 func (s *StudentService) DeleteStudent(ctx *gofr.Context, studentID int) error {
 	return s.Repo.DeleteStudent(ctx, studentID)
 }
